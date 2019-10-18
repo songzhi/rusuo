4c6fb29c907622ccb1d9d43ecf1667e656643745
lsongzhi <lsongzhi@163.com>
2019-10-18 15:03:24 +0800

    feat: example

--- src/rshttp/src/main.rs
+++ src/rshttp/src/main.rs
@@ -6,5 +6,6 @@ use async_std::task;
 use static_http_server::start_server;
 
 fn main() {
-    task::block_on(start_server(SocketAddr::from_str("127.0.0.1:8080").unwrap())).unwrap();
+    println!("Server running on: http://127.0.0.1:8888");
+    task::block_on(start_server(SocketAddr::from_str("127.0.0.1:8888").unwrap())).unwrap();
 }
