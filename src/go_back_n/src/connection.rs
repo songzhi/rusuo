104aa41205a4e031585dbdfd748c876f66ecbcdc
lsongzhi <lsongzhi@163.com>
2019-10-28 14:49:59 +0800

    refactor

--- src/go_back_n/src/connection.rs
+++ src/go_back_n/src/connection.rs
@@ -1,14 +1,35 @@
 use std::cmp::min;
 use std::collections::VecDeque;
 use std::io::Result;
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::Sender;
 use std::time::{Duration, Instant};
 
-use zerocopy::{AsBytes, ByteSlice};
+use zerocopy::AsBytes;
 
 use super::packet::{Header, Packet};
 
 pub const TIMEOUT_DURATION: Duration = Duration::from_secs(3);
+
+pub enum PacketWrapper {
+    Left(Box<[u8]>),
+    Right(Box<[u8]>),
+}
+
+impl PacketWrapper {
+    pub fn new(data: Box<[u8]>, is_left_side: bool) -> Self {
+        if is_left_side {
+            PacketWrapper::Left(data)
+        } else {
+            PacketWrapper::Right(data)
+        }
+    }
+    pub fn is_left_side(&self) -> bool {
+        match self {
+            PacketWrapper::Left(_) => true,
+            PacketWrapper::Right(_) => false
+        }
+    }
+}
 
 pub struct Connection {
     send: SendSequenceSpace,
@@ -16,16 +37,16 @@ pub struct Connection {
     pub(crate) incoming: VecDeque<u8>,
     timer: Option<Instant>,
     unacked: VecDeque<Box<[u8]>>,
-    unsent: VecDeque<u8>,
+    pub(crate)unsent: VecDeque<u8>,
 
-    rx: Receiver<Box<[u8]>>,
-    tx: Sender<Box<[u8]>>,
+    is_left_side: bool,
+    tx: Sender<PacketWrapper>,
 }
 
 
 impl Connection {
     pub const MAX_BODY_SIZE: u32 = 1024;
-    pub fn new(rx: Receiver<Box<[u8]>>, tx: Sender<Box<[u8]>>) -> Self {
+    pub fn new(is_left_side: bool, tx: Sender<PacketWrapper>) -> Self {
         Self {
             send: SendSequenceSpace::new(1),
             recv: RecvSequenceSpace::new(1),
@@ -33,47 +54,37 @@ impl Connection {
             timer: None,
             unacked: VecDeque::new(),
             unsent: VecDeque::new(),
-            rx,
+            is_left_side,
             tx,
         }
     }
-    pub fn start_loop(&mut self) -> Result<()> {
-        loop {
-            if self.send.is_sendable() && self.unsent.len() > 0 {
-                let body_len = min(Self::MAX_BODY_SIZE as usize, self.unsent.len());
-                let header = Header::new(self.send.get_next_seq_num_then_inc(), body_len as u32, false);
-                let packet = header.as_bytes().iter().copied().chain(self.unsent.drain(..body_len)).collect::<Box<_>>();
-                self.tx.send(packet.clone()).unwrap();
-                self.unacked.push_back(packet);
-                self.reset_timer();
-            }
-            if let Ok(packet) = self.rx.recv_timeout(Duration::from_millis(10)) {
-                self.on_packet(packet.as_bytes());
-            }
-            self.on_tick()?;
-        };
-    }
+
     pub fn on_tick(&mut self) -> Result<()> {
+        if self.send.is_sendable() && !self.unsent.is_empty() {
+            let body_len = min(Self::MAX_BODY_SIZE as usize, self.unsent.len());
+            let header = Header::new(self.send.get_next_seq_num_then_inc(), body_len as u32, false);
+            let packet = header.as_bytes().iter().copied().chain(self.unsent.drain(..body_len)).collect::<Box<_>>();
+            self.tx.send(PacketWrapper::new(packet.clone(), self.is_left_side)).unwrap();
+            self.unacked.push_back(packet);
+            self.reset_timer();
+        }
         if let Some(timeout) = self.timer {
             if timeout >= Instant::now() {
-                self.resend_unacked(self.send.unacked_count())?;
+                self.reset_timer();
+                for packet in self.unacked.iter() {
+                    self.tx.send(PacketWrapper::new(packet.clone(), self.is_left_side)).expect("Send failed");
+                };
             }
         }
         Ok(())
     }
+    #[inline]
     fn reset_timer(&mut self) {
         self.timer = Some(Instant::now() + TIMEOUT_DURATION);
     }
 
-    pub fn resend_unacked(&mut self, count: usize) -> Result<()> {
-        self.reset_timer();
-        for packet in self.unacked.iter().take(count) {
-            self.tx.send(packet.clone()).unwrap();
-        };
-        Ok(())
-    }
-    pub fn on_packet<B: ByteSlice>(&mut self, bytes: B) {
-        if let Some(packet) = Packet::parse(bytes) {
+    pub fn on_packet(&mut self, packet: Box<[u8]>) {
+        if let Some(packet) = Packet::parse(packet.as_ref()) {
             if packet.is_ack() {
                 let acked_count = self.send.ack(packet.get_seq_num());
                 drop(self.unacked.drain(..acked_count));
@@ -82,7 +93,8 @@ impl Connection {
                 }
             } else if self.recv.rcv(packet.get_seq_num()) {
                 let ack_header = Header::new(packet.get_seq_num(), 0, true);
-                self.tx.send(ack_header.as_bytes().iter().copied().collect::<Box<_>>()).unwrap();
+                let ack_packet = PacketWrapper::new(ack_header.as_bytes().iter().copied().collect::<Box<_>>(), self.is_left_side);
+                self.tx.send(ack_packet).expect("Send ACK failed");
                 self.incoming.extend(packet.body.iter().take(packet.get_body_len() as usize));
             }
         }
